197e39dce67f4ddf2ba504ea644ce7f1f050af41
Maciej Mazek <maciejmazek@gmail.com>
2023-04-24 16:21:53 +0200

    Rozpisanie testów dla cart

---
+++ cypress/e2e/toolsQa/cart.cy.js
@@ -0,0 +1,161 @@
+describe("Testy koszyka sklepowego", () => {
+    context("Testy ikony koszyka", () => {
+        it("Kliknięcie na ikonkę koszyka w pustym cart", () => {
+
+        });
+
+        it("Kliknięcie na tekst koszyka w pustyn cart", () => {
+
+        });
+
+        it("Dodanie przedmiotu do cart", () => {
+
+        });
+
+        it("Kliknięcie w ikonkę cart z przedmiotem", () => {
+
+        });
+
+        it("Dodanie kilku przedmiotów naraz", () => {
+
+        });
+
+        it("Dodanie wielu nowych przedmiotów do koszyka zawierającego ten przedmiot", () => {
+
+        });
+
+        it("Dodanie wielu przedmiotu x do koszyka z przedmiotami y", () => {
+
+        });
+
+        it("Dodanie wielu przedmiotów korzystając z pola do wpisywania", () => {
+
+        });
+
+        it("Odświeżenie strony głównej mając przedmioty w koszyku", () => {
+
+        });
+
+        it("Usunięcie i dodanie innego przedmiotu", () => {
+
+        });
+
+        it("Kliknięcie na 'add to cart' bez wybrania z combo", () => {
+
+        });
+
+        it("Kliknięcie na 'add to cart' z jednym z dwóch combo wybranych", () => {
+
+        });
+    });
+
+    context("Testy strony /cart", () => {
+        it("Dodanie przedmiotu i przejście na /cart", () => {
+
+        });
+        it("Kliknięcie w 'return to shop", () => {
+
+        });
+        it("Dodanie większej ilości przedmiotu do już dodanego i przejście na /cart", () => {
+
+        });
+        it("Dodanie innego przedmiotu do już istniejących i przejście na /cart", () => {
+
+        });
+        it("Usunięcie jednego przedmiotu używając - na /cart", () => {
+
+        });
+        it("Usunięcie jednego przedmiotu używając klikając na update cart", () => {
+
+        });
+        it("Usunięcie jednego z przedmiotów mających większą niż 1 ilość i f5 na /cart", () => {
+
+        });
+        it("Usunięcie jednego z przedmiotów mających większą niż 1 ilość i f5 na /cart - klikając na update cart", () => {
+
+        });
+        it("Usunięcie wszystkich przedmiotów używając - na /cart", () => {
+
+        });
+        it("Usunięcie po jednym przedmiocie z X różnych opcji", () => {
+
+        });
+        it("Usunięcie po jednym przedmiocie z X różnych opcji klikając na update cart", () => {
+
+        });
+        it("Usunięcie przedmiotów używając (x) obok przedmiotu", () => {
+
+        });
+        it("Undo po usunięciu całego kafla z przedmiotami używając (x)", () => {
+
+        });
+        it("Dodanie przedmiotu korzystając z +", () => {
+
+        });
+        it("Dodanie przedmiotu klikając na update cart", () => {
+
+        });
+
+        it("f5 po dodaniu przedmiotu korzystając z +", () => {
+
+        });
+
+        it("f5 po dodaniu przedmiotu klikając na update cart", () => {
+
+        });
+        it("Zmiana ilości przedmiotu poprzez wpisanie wartości - mniej", () => {
+
+        });
+        it("Zmiana ilości przedmiotu poprzez update cart", () => {
+
+        });
+        it("Zmiana ilości przedmiotu poprzez wpisanie wartości - więcej", () => {
+
+        });
+        it("Zmiana ilości przedmiotu poprzez wpisanie wartości - więcej z update cart", () => {
+
+        });
+        it("Zmiana ilości przedmiotu poprzez wpisanie bardzo dużej wartości", () => {
+
+        });
+        it("Zmiana ilości przedmiotu poprzez wpisanie bardzo dużej wartości - update cart", () => {
+
+        });
+        it("Kliknięcie clear shopping cart z 1 przedmiotem", () => {
+
+        });
+        it("Kliknięcie clear shopping cart z dwoma przedmiotami", () => {
+
+        });
+
+        it("Kliknięcie Continue shopping", () => {
+
+        });
+
+        it("Kliknięcie Proceed to checkout", () => {
+
+        });
+
+        it("Kliknięcie ToolsQa Demo Site", () => {
+
+        });
+    });
+
+    context("Testy funkcji 'discount'", () => {
+        it("Nieprawidłowy kupon", () => {
+
+        });
+
+        it("Prawidłowy kupon", () => {
+
+        });
+
+        it("Jak nie ma w koszyku produktu to nie pojawia się discount", () => {
+
+        });
+
+        it("Jak jest produkt w koszyku to pojawia się discount", () => {
+
+        });
+    });
+});
